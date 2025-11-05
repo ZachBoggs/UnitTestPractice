57e093b0af75c7c9a5b742b9431099a8b3401fc0
Zach Boggs <55774849+ZachBoggs@users.noreply.github.com>
2025-11-05 11:44:26 -0800

    fix(PasswordTest): added more unit tests
    added more unit tests to provide a more thorough testof the code

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,67 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+
+TEST(PasswordTest, no_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, single_letter_lower_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("z");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, single_letter_upper_password)
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, multi_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZZZZ");
+	ASSERT_EQ(4, actual);
+}
+
+TEST(PasswordTest, upper_and_lower_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("aaaAAAaAA");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, number_test_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("1234321");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, longer_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("7777777656777");
+	ASSERT_EQ(7, actual);
+}
+
+TEST(PasswordTest, multi_number_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("33322333");
+	ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, three_leading_space_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("   threespaces");
+	ASSERT_EQ(3, actual);
+}
+
